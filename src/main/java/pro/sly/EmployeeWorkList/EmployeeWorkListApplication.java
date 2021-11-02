61de6b9f4b38187d571b697a677cb2ac21f06d70
_.danloncer._ <ermolaev.daniil2004@mail.ru>
2021-11-02 16:18:22 +0300

    Initial commit

---
+++ src/main/java/pro/sly/EmployeeWorkList/EmployeeWorkListApplication.java
@@ -0,0 +1,13 @@
+package pro.sly.EmployeeWorkList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWorkListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWorkListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sly/EmployeeWorkList/EmployeeWorkListApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sly.EmployeeWorkList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeWorkListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
